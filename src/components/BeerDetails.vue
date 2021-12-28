588d7839d0a0936d70dff171da5c7714d24beabf
BluABK <bluabk@bluabk.net>
2021-12-28 13:45:02 +0100

    Worked on BeerDetails.
    - Centered and improved layout.
    - Implemented embedding of ingredient states into the beer object itself.
    - Added and started work on ingredient processing buttons (2-state and timer).
    - Lots of CSS back-and-forth to improve looks.

--- src/components/BeerDetails.vue
+++ src/components/BeerDetails.vue
@@ -1,68 +1,75 @@
 <template>
   <div class="beer-details-screen" v-show="isVisible">
-    <button @click="showBeerList">Back to list</button>
-    <h1>{{ msg }}</h1>
+    <div class="beer-details-screen-content">
+      <button @click="showBeerList">Back to list</button>
+      <h1>{{ msg }}</h1>
 
-    <div class="beer-item" >
-      <img class="beer-image" alt="Beer image" v-bind:src="beerImageUrl">
-      <div class="beer-item-content">
-        <div class="beer-item-name">
-          {{beerName}}
-        </div>
-        <div class="beer-item-spacer"></div>
-        <div class="beer-item-tagline">
-          {{beerTagline}}
-        </div>
-        <div class="beer-item-spacer"></div>
-        <div class="beer-item-abv">ABV: {{beerAbv}}</div>
-        <div class="beer-item-spacer"></div>
-        <div class="beer-item-description"><p>{{beerDescription}}</p></div>
-        <div class="beer-item-hops" v-if="beerIngredientsHops.length > 0">
-          Hops:
-          <ul>
-            <li v-for="(hop, i) in beerIngredientsHops" v-bind:key="'hops' + i + hop.name">
-              <p>Name: {{hop.name}}</p>
-              <p>Amount: {{hop.amount.value}} {{hop.amount.unit}}</p>
-              <p>Add: {{hop.add}}</p>
-              <p>Attribute: {{hop.attribute}}</p>
-            </li>
-          </ul>
-        </div>
-        <div class="beer-item-malts" v-if="beerIngredientsMalts.length > 0">
-          Malts:
-          <ul>
-            <li v-for="(malt, i) in beerIngredientsMalts" v-bind:key="'malt' + i + malt.name">
-              <p>Name: {{malt.name}}</p>
-              <p>Amount: {{malt.amount.value}} {{malt.amount.unit}}</p>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="beer-item-methods">
-        Methods:
-        <ul>
-          <li v-if="beerMethods.mash_temp.length > 0">
-            <p>Mash temperatures:</p>
-            <ul>
-              <li v-for="(mashTemp, i) in beerMethods.mash_temp" v-bind:key="'mash_temp' + i + mashTemp.temp.value">
-                <p>{{mashTemp.duration}} minutes at {{mashTemp.temp.value}} {{mashTemp.temp.unit}}.</p>
-              </li>
-            </ul>
-          </li>
+      <div class="beer-item" >
+        <img class="beer-image" alt="Beer image" v-bind:src="beerImageUrl">
+        <div class="beer-item-content">
+          <div class="beer-item-name">
+            {{beerName}}
+          </div>
+          <div class="beer-item-spacer"></div>
+          <div class="beer-item-tagline">
+            {{beerTagline}}
+          </div>
+          <div class="beer-item-spacer"></div>
+          <div class="beer-item-abv">ABV: {{beerAbv}}</div>
+          <div class="beer-item-spacer"></div>
+          <div class="beer-item-description"><p>{{beerDescription}}</p></div>
+          <div class="ingredients">
+            <div class="ingredients-content">
+              <div class="hops ingredients-element" v-if="beerIngredientsHops.length > 0">
+                <div class="hops-heading ingredients-heading">Hops</div>
+                  <div class="hops-content" v-for="(hop, i) in beerIngredientsHops" v-bind:key="'hops' + i + hop.name">
+                    <p>
+                      <button class="double-state-button app-button" @click="clickedTwoStateButton($event, hop)">
+                        {{hop.state}}
+                      </button>
+                      [Add: {{hop.add}}] {{hop.amount.value}} {{hop.amount.unit}} {{hop.name}} ({{hop.attribute}})
+                    </p>
+                  </div>
+              </div>
+              <div class="malts ingredients-element" v-if="beerIngredientsMalts.length > 0">
+                <div class="malts-heading ingredients-heading">Malts</div>
+                  <div class="malts-content" v-for="(malt, i) in beerIngredientsMalts" v-bind:key="'malt' + i + malt.name">
+                    <p>
+                      <button class="double-state-button app-button" @click="clickedTwoStateButton($event, malt)">
+                        {{malt.state}}
+                      </button>
+                      {{malt.amount.value}} {{malt.amount.unit}} {{malt.name}}
+                    </p>
+                  </div>
+              </div>
+              <div class="methods ingredients-element">
+                <div class="methods-heading ingredients-heading">Methods</div>
+                  <div v-if="beerMethods.mash_temp.length > 0">
+                    <div class="methods-mashtemp-heading">
+                      <p>Mash temperatures:</p>
+                    </div>
+                    <div class="methods-mashtemp-content" v-for="(mashTemp, i) in beerMethods.mash_temp" v-bind:key="'mash_temp' + i + mashTemp.temp.value">
+                      <p>
+                        <button class="timer-button app-button" @click="clickedTimerButton($event, mashTemp)" v-bind:value="mashTemp.duration">{{mashTemp.state}}</button> {{mashTemp.duration}} minutes at {{mashTemp.temp.value}} {{mashTemp.temp.unit}}.
+                        <span class="timer"></span>
+                      </p>
+                    </div>
+                  </div>
 
-          <li v-if="beerMethods.fermentation">
-            <p>Fermentation: {{beerMethods.fermentation.temp.value}} {{beerMethods.fermentation.temp.unit}}.</p>
-          </li>
+                  <div class="methods-fermentation" v-if="beerMethods.fermentation">
+                    <p>Fermentation: <button class="double-state-button app-button" @click="clickedTwoStateButton($event, beerMethods.fermentation)">{{beerMethods.fermentation.state}}</button>{{beerMethods.fermentation.temp.value}} {{beerMethods.fermentation.temp.unit}}.</p>
+                  </div>
 
-          <li v-if="beerMethods.twist">
-            <p>Twist: {{beerMethods.twist}}.</p>
-          </li>
-        </ul>
-
-      </div>
-    </div>
-
-  </div>
+                  <div class="methods-twist" v-if="beerMethods.twist">
+                    <p>Twist: {{beerMethods.twist}}.</p>
+                  </div>
+              </div> <!-- beer-item-methods //-->
+            </div> <!-- ingredients-content //-->
+          </div> <!-- ingredients //-->
+        </div> <!-- beer-item-content //-->
+      </div> <!-- beer-item //-->
+    </div>  <!-- beer-details-screen-content //-->
+  </div>  <!-- beer-details-screen  //-->
 </template>
 
 <script>
@@ -112,7 +119,14 @@ export default {
     beerIngredientsHops() {
       if (this.selectedBeer != null && Object.hasOwn(this.selectedBeer, "ingredients")) {
         if (this.selectedBeer.ingredients != null && Object.hasOwn(this.selectedBeer.ingredients, "hops")) {
-          return this.selectedBeer.ingredients.hops;
+          // Add in custom tracking of processing each item
+          let hops = this.selectedBeer.ingredients.hops;
+
+          for (let hop of hops) {
+            hop["state"] = "IDLE";
+          }
+
+          return hops;
         }
       }
 
@@ -121,19 +135,46 @@ export default {
     beerIngredientsMalts() {
       if (this.selectedBeer != null && Object.hasOwn(this.selectedBeer, "ingredients")) {
         if (this.selectedBeer.ingredients != null && Object.hasOwn(this.selectedBeer.ingredients, "malt")) {
-          return this.selectedBeer.ingredients.malt;
+          // Add in custom tracking of processing each item
+          let malts = this.selectedBeer.ingredients.malt;
+
+          for (let malt of malts) {
+            malt["state"] = "IDLE";
+          }
+
+          return malts;
         }
       }
 
       return [];
     },
     beerMethods() {
-      if (this.selectedBeer != null && Object.hasOwn(this.selectedBeer, "method")) return this.selectedBeer.method;
+      let mashTemps = [];
+      let fermentation = null;
+      let twist = null;
+
+      if (this.selectedBeer != null && Object.hasOwn(this.selectedBeer, "method")) {
+        fermentation = this.selectedBeer.method.fermentation;
+        twist = this.selectedBeer.method.twist;
+
+        if (Object.hasOwn(this.selectedBeer.method, "mash_temp")) {
+          if (this.selectedBeer.method.mash_temp.length > 0) {
+            // Add in custom tracking of processing each item
+            mashTemps = this.selectedBeer.method.mash_temp;
+
+            for (let mashTemp of mashTemps) {
+              mashTemp["state"] = "IDLE";
+              // Convert duration in minutes to more easily computable seconds.
+              mashTemp["time_remaining"] = mashTemp.duration * 3600
+            }
+          }
+        }
+      }
 
       return {
-        mash_temp: [],
-        fermentation: null,
-        twist: null
+        mash_temp: mashTemps,
+        fermentation: fermentation,
+        twist: twist
       };
     }
   },
@@ -143,10 +184,28 @@ export default {
     }
   },
   methods: {
+    // Utils
+    uuid: function() {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    },
+    // Own
     showBeerList: function(beer) {
       this.$emit('showBeerList');
       this.$emit('selectBeer', null);
       console.info("Show beer list", beer);
+    },
+    clickedTwoStateButton: function (event, obj) {
+      obj.state = "DONE";
+      event.target.textContent = obj.state;
+    },
+    clickedTimerButton: function (event, obj) {
+      console.log(event);
+      console.log(obj);
+      obj.state = "RUNNING";
+      event.target.textContent = obj.state;
     }
   }
 }
@@ -158,8 +217,78 @@ ul {
   /*float:left;*/
 }
 
+.beer-details-screen {
+  text-align:center;
+}
+
+.beer-details-screen-content {
+  margin: auto;
+  width:80vw;
+}
+
+.beer-item {
+}
+
+.beer-item-content {
+}
+
 .beer-item img {
-  height: 100px;
+  height: 200px;
+}
+
+.hops {
+  /*background-color: mediumpurple;*/
+  float: left;
+  width: 33.3%;
+  /*border-left: 1px solid black;*/
+}
+
+.malts {
+  /*background-color: coral;*/
+  float: left;
+  width: 33.3%;
+  /*border-left: 1px solid black;*/
+}
+
+.methods {
+  /*background-color: cornflowerblue;*/
+  float: left;
+  width: 33.3%;
+  /*border-left: 1px solid black;*/
+  /*border-right: 1px solid black;*/
+}
+
+.ingredients-element {
+  /*padding: 0 10px 0 10px;*/
+}
+
+.hops-content {
+  /*font-weight: bold;*/
+}
+
+.ingredients {
+  /*margin: auto;*/
+  text-align:center;
+  /*height:300px;*/
+  /*width:80vw;*/
+  /*overflow: scroll;*/
+
+}
+
+.ingredients-content {
+  /*height:100%;*/
+  /*height: 50vh;*/
+  width:100%;
+  margin: auto;
+  text-align: left;
+  /*background-color: #b7b7b7;*/
+  background-color: mediumpurple;
+}
+
+.ingredients-heading {
+  text-align: center;
+  font-weight: bold;
+  font-size: 125%;
 }
 
 </style>
